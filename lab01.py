55d5ffbd39f793cc370b9c7ae724d23ed875e248
mehsheed <mehsheed59@gmail.com>
2023-09-14 13:23:01 -0600

    Final commit

--- lab01.py
+++ lab01.py
@@ -4,5 +4,8 @@ print(requests.__version__)
 
 r = requests.get("http://google.com")
 
-print (r.text)
+#print (r.text)
 
+raw = requests.get("https://raw.githubusercontent.com/mehsheed/CMPUT404/main/lab01.py")
+
+print (raw.text)
